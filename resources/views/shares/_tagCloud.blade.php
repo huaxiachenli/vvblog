f6956535a62839cb6d3c4e4c4aaa4f1e33238c43
huaxiachenli <huaxiachenli@163.com>
2017-07-31 08:48:39 +0800

    标签云优化

--- resources/views/shares/_tagCloud.blade.php
+++ resources/views/shares/_tagCloud.blade.php
@@ -2,8 +2,8 @@
     <div class="panel-heading"><i class="fa fa-cloud" aria-hidden="true"></i> 标签云</div>
     <div class="panel-body">
         @foreach($user->tags()->pluck('name') as $tag)
-            <a href="{{ url()->route('tag',[$user->id,$tag]) }}">
-                <span for="label"  style="margin-bottom: 20px;" class="label {{ array_rand(['label-default'=>0,'label-primary'=>1,'label-danger'=>2,'label-success'=>3,'label-info'=>4]) }}">{{ $tag }}</span>
+            <a href="{{ url()->route('tag',[$user->id,$tag]) }}" style="margin-left: 10px;line-height:2;"  >
+                <span for="label"  class="label {{ array_rand(['label-default'=>0,'label-primary'=>1,'label-danger'=>2,'label-success'=>3,'label-info'=>4]) }}">{{ $tag }}</span>
             </a>
         @endforeach
     </div>
